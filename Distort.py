676461de6484a40a1d43dfaa25c77ceb989bd7cb
Jason Knoll <22356211+jasonknoll@users.noreply.github.com>
2025-01-28 11:16:59 -0500

    Create Distort.py

---
+++ Distort.py
@@ -0,0 +1,10 @@
+# Librosa Distortion + Xavier's Dyanmic Compressor
+
+'''
+  TODO
+  Load audio
+  Compress w/ compress.py -- process_audio() -- Compress before or after?
+  Take again and amplify
+  Clip the magnitude
+  Output audio
+'''
